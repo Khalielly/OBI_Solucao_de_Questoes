72e291cc3294bb8f5a3f277c0e7721b436bec1d6
Danielly Khalil Cândido Teixeira <152915978+Khalielly@users.noreply.github.com>
2024-05-21 16:53:26 -0300

    up

--- 2014/Fase 1/cartas.py
+++ 2014/Fase 1/cartas.py
@@ -6,15 +6,15 @@ num_cartas = num_cartas.split()
 for i in range(len(num_cartas) - 1):
     if num_cartas[i] < num_cartas[i + 1]:
         ordem = 'C'
-    elif num_cartas[i] > num_cartas[i - 1]:
+    elif num_cartas[i] < num_cartas[i - 1]:
         ordem = 'D'
     else:
         ordem = 'N'
         break
 
 if ordem == 'C':
-    print(ordem)
+    print(f'{ordem}\n')
 elif ordem == 'D':
-    print(ordem)
+    print(f'{ordem}\n')
 else:
-    print(ordem)
+    print(f'{ordem}\n')
