2faa8be1375f04946d0afd461c1b2423bb7c9b57
Danielly Khalil Cândido Teixeira <152915978+Khalielly@users.noreply.github.com>
2024-05-22 15:31:59 -0300

    up

---
+++ 2014/Fase 1/carteiro.py
@@ -0,0 +1,20 @@
+N, M = [int(d) for d in input().split()]
+casas = [int(c) for c in input().split()]
+encomendas = [int(e) for e in input().split()]
+
+
+tempo = 0 
+local_atual = 0   
+index = {}
+i = 0
+
+for casa in casas:
+    index[casa] = i
+    i+= 1
+
+for encomenda in encomendas:
+    tempo += abs(index[encomenda] - local_atual) 
+    local_atual = index[encomenda] 
+    
+print(tempo)
+
