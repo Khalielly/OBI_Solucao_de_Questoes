060d4d559c438d8a80fb81ac2b761a992f18fe0b
Danielly Khalil Cândido Teixeira <152915978+Khalielly@users.noreply.github.com>
2024-06-09 18:27:21 -0300

    questão 1 respondida

---
+++ ProvaOBI2023/conta.py
@@ -0,0 +1,19 @@
+# Prova da Olimpíada Brasileira de Informática 2023 (https://olimpiada.ic.unicamp.br/static/extras/obi2023/provas/ProvaOBI2023_f1ps.pdf)
+
+valor = int(input())
+A = int(input())
+F = int(input())
+P = int(input())
+
+
+contas=[A,F,P]
+contas.sort()
+contasPg = 0
+
+for conta in contas:
+    if valor >= conta:
+        valor-=conta
+        contasPg+=1
+
+print(contasPg)
+
